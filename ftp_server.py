b21095cb8f7663464f5e9c04381d009a792fc5eb
MaPing01 <pingpingma001@163.com>
2018-09-27 16:46:20 +0800

    ftp server complete add comment

--- ftp_server.py
+++ ftp_server.py
@@ -2,6 +2,14 @@ import socketserver
 import os
 import json
 
+
+'''
+1. 创建一个请求处理的类，并且这个类要继承 BaseRequestHandlerclass ，并且还要重写父类里handle()方法；
+2. 你必须实例化 TCPServer，并且传递server IP和你上面创建的请求处理类，给这个TCPServer；
+3. server.handle_requese()#只处理一个请求，server.server_forever()处理多个一个请求，永远执行
+4. 关闭连接server_close()
+'''
+
 class FtpHandler(socketserver.BaseRequestHandler):
 
     def cmd_put( self,*args ):
